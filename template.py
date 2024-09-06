c35ddd1ee5f13f4b8e73a09630fea300b4e45dc4
LWX270901 <letungocminh@gmail.com>
2024-09-06 16:04:19 +0900

    requirements added

--- template.py
+++ template.py
@@ -8,20 +8,21 @@ project_name = "cnnClassifier"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
-    f"src{project_name}/__init__.py",
-    f"src{project_name}/components/__init__.py",
-    f"src{project_name}/utils/__init__.py",
-    f"src{project_name}/config/__init__.py",
-    f"src{project_name}/config/configuration.py",
-    f"src{project_name}/pipeline/__init__.py",
-    f"src{project_name}/enity/__init__.py",
-    f"src{project_name}/constants/__init__.py",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/enity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
-    "dvc/yaml",
+    "dvc.yaml",
     "params.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
+    "templates/index.html"
 ]
 
 for filepath in list_of_files:
